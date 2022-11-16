f335176f0cd94683140737dc318aad37212f1e36
Marian Villavicencio <marian.mlva.16@gmail.com>
2022-11-16 17:39:34 -0600

    first steps

---
+++ burguerqueen/src/App.js
@@ -0,0 +1,20 @@
+import queen from './queen.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={queen} className="App-logo" alt="logo" />
+        <p className="App-orange">
+          Burger Queen WorkSpace
+        </p>
+        <p>
+          Coming soon ... 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
