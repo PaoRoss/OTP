a620ba73b497cfcf945628d76ab2a2569d787ffa
PaoRoss <paodiazglez.pdgr@gmail.com>
2023-01-03 17:47:32 -0600

    fix: request

--- burguerqueen/src/components/generalComponents/httpRequests.js
+++ burguerqueen/src/components/generalComponents/httpRequests.js
@@ -4,7 +4,7 @@ const api = 'https://6376d05f81a568fc25067c85.mockapi.io/api/bq6/';
 // también se puede hacer con una función
 
 export const createOrder = (order) => {
-   return axios.post(`${api}order`, order)
+   return axios.post(`${api}orders`, order)
     .then(function (response) {
       console.log(response);
       return { status: response.status }
